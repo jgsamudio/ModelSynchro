c00c31bf143ddf6b989d4d9d715b65bfe083b4eb
Jonathan Samudio <jonathan2457@gmail.com>
2017-11-28 16:16:34 -0500

    Moved protocol to another file.

---
+++ ModelSynchro/ModelSynchro/LanguageFormatter/LanguageFormatter.swift
@@ -0,0 +1,20 @@
+//
+//  LanguageFormatter.swift
+//  ModelSynchro
+//
+//  Created by Jonathan Samudio on 11/28/17.
+//  Copyright © 2017 Jonathan Samudio. All rights reserved.
+//
+
+import Foundation
+
+protocol LanguageFormatter {
+    
+    var fileExtension: String { get }
+    var optional: String { get }
+    var modelClassEndLine: String { get }
+    
+    func fileHeader(name: String, config: ConfigurationFile) -> String
+    func modelClassDeclaration(name: String) -> String
+    func variableString(property: String, type: String) -> String
+}
