65ffdb8f7d8dd4be4f38563c038b4291554a45b7
Jonathan Samudio <jonathan2457@gmail.com>
2018-01-02 17:02:43 -0500

    Added array extensions tests

---
+++ ModelSynchro/ModelSynchroTests/Extensions/ArrayExtensionTests.swift
@@ -0,0 +1,83 @@
+//
+//  ArrayExtensionTests.swift
+//  ModelSynchroTests
+//
+//  Created by Jonathan Samudio on 1/2/18.
+//  Copyright © 2018 Jonathan Samudio. All rights reserved.
+//
+
+import XCTest
+
+@testable import ModelSynchro
+class ArrayExtensionTests: XCTestCase {
+
+    // MARK: Line
+
+    func testLineArrayFind_Found() {
+        let array = [Line(property: "Sample", type: "Type", isOptional: false, customProperty: nil)]
+        let line = Line(property: "Sample", type: "Type", isOptional: false, customProperty: nil)
+
+        let foundLine = array.find(line: line)
+
+        XCTAssertEqual(line, foundLine)
+    }
+
+    func testLineArrayFind_NotFoundType() {
+        let array = [Line(property: "Sample", type: "Type", isOptional: false, customProperty: nil)]
+        let line = Line(property: "Sample", type: "Type2", isOptional: false, customProperty: nil)
+
+        let foundLine = array.find(line: line)
+
+        XCTAssertNotEqual(line, foundLine)
+    }
+
+    func testLineArrayFind_NotFoundProperty() {
+        let array = [Line(property: "Sample", type: "Type", isOptional: false, customProperty: nil)]
+        let line = Line(property: "Sample2", type: "Type", isOptional: false, customProperty: nil)
+
+        let foundLine = array.find(line: line)
+
+        XCTAssertNotEqual(line, foundLine)
+    }
+
+    func testLineArrayFind_FoundOptional() {
+        let array = [Line(property: "Sample", type: "Type", isOptional: true, customProperty: nil)]
+        let line = Line(property: "Sample", type: "Type", isOptional: false, customProperty: nil)
+
+        let foundLine = array.find(line: line)
+
+        XCTAssertEqual(line, foundLine)
+    }
+
+    func testLineArrayFind_FoundCustomProperty() {
+        let custom = CustomProperty(customLine: "Custom", property: "Prop", keyedProperty: nil)
+        let array = [Line(property: "Sample", type: "Type", isOptional: false, customProperty: custom)]
+        let line = Line(property: "Sample", type: "Type", isOptional: false, customProperty: nil)
+
+        let foundLine = array.find(line: line)
+
+        XCTAssertEqual(line, foundLine)
+    }
+
+    // MARK: Custom Property
+
+    func testCustomPropertyFind_Found() {
+        let keyedProp = KeyedProperty(mappedProperty: "Prop", jsonProperty: "JSON")
+        let array = [CustomProperty(customLine: "Custom", property: "Prop", keyedProperty: keyedProp)]
+        let customProperty = CustomProperty(customLine: "Custom", property: "Prop", keyedProperty: keyedProp)
+
+        let foundProperty = array.find(property: "JSON")
+
+        XCTAssertEqual(foundProperty?.keyedProperty?.jsonProperty, customProperty.keyedProperty?.jsonProperty)
+    }
+
+    func testCustomPropertyFind_NotFound() {
+        let keyedProp = KeyedProperty(mappedProperty: "Prop", jsonProperty: "JSON")
+        let array = [CustomProperty(customLine: "Custom", property: "Prop", keyedProperty: keyedProp)]
+        let customProperty = CustomProperty(customLine: "Custom", property: "Prop", keyedProperty: keyedProp)
+
+        let foundProperty = array.find(property: "JSON2")
+
+        XCTAssertNotEqual(foundProperty?.keyedProperty?.jsonProperty, customProperty.keyedProperty?.jsonProperty)
+    }
+}
