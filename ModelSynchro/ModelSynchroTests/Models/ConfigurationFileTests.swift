8fb7401948a447551c08b1e0bd3fbead6ab8a1e2
Jonathan Samudio <jonathan2457@gmail.com>
2018-05-15 11:05:01 -0400

    Added unit tests for the mapped configuration file.

--- ModelSynchro/ModelSynchroTests/Models/ConfigurationFileTests.swift
+++ ModelSynchro/ModelSynchroTests/Models/ConfigurationFileTests.swift
@@ -6,4 +6,122 @@
 //  Copyright © 2018 Jonathan Samudio. All rights reserved.
 //
 
-import Foundation
+import XCTest
+
+class ConfigurationFileTests: XCTestCase {
+
+    var sut: ConfigurationFile!
+
+    func testMappedJsonKey_NilMappings() {
+        let key = "jsonKey"
+        sut = ConfigurationFile(authorName: "Author",
+                                companyName: "Company",
+                                projectName: "MyCoolProject",
+                                language: "Swift",
+                                outputDirectory: "/somewhere",
+                                endpoints: [],
+                                headers: nil,
+                                authEndpoint: nil,
+                                localJSONDirectory: nil,
+                                mappedModelNames: nil)
+
+        let result = sut.mapped(jsonKey: key)
+
+        XCTAssertEqual(result, key)
+    }
+
+    func testMappedJsonKey_NotFound() {
+        let key = "jsonKey"
+        let mappedModelInfo = MappedModelInfo(jsonKey: "OtherJsonKey", fileName: nil, mappedName: "MappedName")
+        sut = ConfigurationFile(authorName: "Author",
+                                companyName: "Company",
+                                projectName: "MyCoolProject",
+                                language: "Swift",
+                                outputDirectory: "/somewhere",
+                                endpoints: [],
+                                headers: nil,
+                                authEndpoint: nil,
+                                localJSONDirectory: nil,
+                                mappedModelNames: [mappedModelInfo])
+
+        let result = sut.mapped(jsonKey: key)
+
+        XCTAssertEqual(result, key)
+    }
+
+    func testMappedJsonKey_Found() {
+        let key = "jsonKey"
+        let mappedModelInfo = MappedModelInfo(jsonKey: key, fileName: nil, mappedName: "MappedName")
+        sut = ConfigurationFile(authorName: "Author",
+                                companyName: "Company",
+                                projectName: "MyCoolProject",
+                                language: "Swift",
+                                outputDirectory: "/somewhere",
+                                endpoints: [],
+                                headers: nil,
+                                authEndpoint: nil,
+                                localJSONDirectory: nil,
+                                mappedModelNames: [mappedModelInfo])
+
+        let result = sut.mapped(jsonKey: key)
+
+        XCTAssertEqual(result, mappedModelInfo.mappedName)
+    }
+
+    func testMappedFileName_NilMappings() {
+        let fileName = "fileName"
+        sut = ConfigurationFile(authorName: "Author",
+                                companyName: "Company",
+                                projectName: "MyCoolProject",
+                                language: "Swift",
+                                outputDirectory: "/somewhere",
+                                endpoints: [],
+                                headers: nil,
+                                authEndpoint: nil,
+                                localJSONDirectory: nil,
+                                mappedModelNames: nil)
+
+        let result = sut.mapped(filename: fileName)
+
+        XCTAssertEqual(result, fileName)
+    }
+
+    func testMappedFileName_NotFound() {
+        let fileName = "fileName"
+        let mappedModelInfo = MappedModelInfo(jsonKey: nil, fileName: "OtherFileName", mappedName: "MappedName")
+        sut = ConfigurationFile(authorName: "Author",
+                                companyName: "Company",
+                                projectName: "MyCoolProject",
+                                language: "Swift",
+                                outputDirectory: "/somewhere",
+                                endpoints: [],
+                                headers: nil,
+                                authEndpoint: nil,
+                                localJSONDirectory: nil,
+                                mappedModelNames: [mappedModelInfo])
+
+        let result = sut.mapped(filename: fileName)
+
+        XCTAssertEqual(result, fileName)
+    }
+
+    func testMappedFileName_Found() {
+        let fileName = "fileName"
+        let mappedModelInfo = MappedModelInfo(jsonKey: nil, fileName: fileName, mappedName: "MappedName")
+        sut = ConfigurationFile(authorName: "Author",
+                                companyName: "Company",
+                                projectName: "MyCoolProject",
+                                language: "Swift",
+                                outputDirectory: "/somewhere",
+                                endpoints: [],
+                                headers: nil,
+                                authEndpoint: nil,
+                                localJSONDirectory: nil,
+                                mappedModelNames: [mappedModelInfo])
+
+        let result = sut.mapped(filename: fileName)
+
+        XCTAssertEqual(result, mappedModelInfo.mappedName)
+    }
+
+}
