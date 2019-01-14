6b8a67c38a52e13bd5e3de211fa900ec1333cf0b
jgsamudio <jonathan2457@gmail.com>
2019-01-14 11:13:15 -0500

    Updated configuration for kotlin.

--- ModelSynchro/SampleJSONOutput1/UserInfo.swift
+++ ModelSynchro/SampleJSONOutput1/UserInfo.swift
@@ -2,8 +2,8 @@
 //  UserInfo.swift
 //  ModelSynchro
 //
-//  Created by Jonathan Samudio on 05/14/18.
-//  Copyright © 2018 Prolific Interactive. All rights reserved.
+//  Created by Jonathan Samudio on 01/14/19.
+//  Copyright © 2019 Prolific Interactive. All rights reserved.
 //
 
 /*
