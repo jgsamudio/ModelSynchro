89d0a27f937b940833dc1a5a89bb0d374fbf8598
jgsamudio <jonathan2457@gmail.com>
2019-01-24 07:41:19 -0500

    Updated swift version number to 4.2 and added formmatted project directory.

--- ModelSynchro/ModelSynchro/ConfigurationParser.swift
+++ ModelSynchro/ModelSynchro/ConfigurationParser.swift
@@ -29,8 +29,9 @@ final class ConfigurationParser {
             return
         }
     #endif
-        ConfigurationParser.projectDirectory = projectDirectory
-        parseConfigurationFile(projectDirectory: projectDirectory)
+        let formattedProjectDirectory = format(projectDirectory: projectDirectory)
+        ConfigurationParser.projectDirectory = formattedProjectDirectory
+        parseConfigurationFile(projectDirectory: formattedProjectDirectory)
     }
 }
 
@@ -59,5 +60,14 @@ private extension ConfigurationParser {
         configData?.deserializeObject(completion: { (config, _) in
             self.configFile = config
         })
+    }
+    
+    private func format(projectDirectory: String) -> String {
+        if projectDirectory.prefix(2) == "./" {
+            let currentDirectory = FileManager.default.currentDirectoryPath
+            let startIndex = projectDirectory.index(projectDirectory.startIndex, offsetBy: 2)
+            return "\(currentDirectory)/\(String(projectDirectory[startIndex..<projectDirectory.endIndex]))"
+        }
+        return projectDirectory
     }
 }
